84d934247f74e1c3fb1ee2ac6e7d89847c364302
Allen <allen.zhao@student.manchester.ac.uk>
2024-04-13 21:01:09 +0100

    Modified ConstSelection and made it go to StarSystemSelection

--- Godot/Scripts/SelectConstButton.gd
+++ Godot/Scripts/SelectConstButton.gd
@@ -1,14 +1,12 @@
-extends LineEdit
+extends Button
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	grab_focus()
 	pass # Replace with function body.
 
-func _on_LineEdit_text_entered(new_text):
-	UserSettings.save_username(new_text)
-	return new_text
+func _pressed():
+	get_tree().change_scene_to_file("res://scenes/StarSystemSelection.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
