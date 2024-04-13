ec5d3c5291a60e284eae4d1e22931b85f2918d21
Allen <allen.zhao@student.manchester.ac.uk>
2024-04-13 19:47:56 +0100

    Connected the main menu and userinput files together

--- Godot/Scripts/StartButton.gd
+++ Godot/Scripts/StartButton.gd
@@ -3,14 +3,11 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var button = Button.new()
-	button.text = "press me"
-	button.pressed.connect(self._button_pressed)
 	pass # Replace with function body.
 
-func _button_pressed():
-	print("Hello world")
-
+func _pressed():
+	get_tree().change_scene_to_file("res://scenes/NameInput.tscn")
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
