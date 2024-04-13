c386d8ebd21ac9611f45ebe9d2da513e7e74fd47
k27330ms <mirza.shahrilizal@student.manchester.ac.uk>
2024-04-13 23:07:58 +0100

    added asteroids, click-to-start rockets and retry popup

--- Godot/Scripts/Rocket.gd
+++ Godot/Scripts/Rocket.gd
@@ -4,14 +4,20 @@ extends CharacterBody2D
 @export var acceleration = 800
 
 var target = position
+var enable_movement = false
 
 func _input(event):
-	# if event.is_action_pressed("ui_mouse_left"):
+	if event.is_action_pressed("ui_mouse_left"):
+		enable_movement = true
+		get_node("Label").queue_free()
 	pass
 
 func _physics_process(delta):
-	target = get_global_mouse_position()
-	velocity = position.direction_to(target) * speed * position.distance_to(target)
-	look_at(target)
+	if enable_movement:
+		target = get_global_mouse_position()
+		velocity = position.direction_to(target) * speed * position.distance_to(target)
+		look_at(target)
+	else:
+		velocity
 	if position.distance_to(target) > 10:
 		move_and_slide()
