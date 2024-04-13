d08b87ad57b4e4689b4a65e6ad9f480614bd3533
k27330ms <mirza.shahrilizal@student.manchester.ac.uk>
2024-04-13 21:09:10 +0100

    username is saved

--- Godot/Scripts/ConfirmNameButton.gd
+++ Godot/Scripts/ConfirmNameButton.gd
@@ -6,6 +6,10 @@ func _ready():
 	pass # Replace with function body.
 
 func _pressed():
+	# Assuming the LineEdit node is a sibling of this Button
+	var line_edit = get_parent().get_node("LineEdit")
+	var username = line_edit.text
+	UserSettings.save_username(username)
 	get_tree().change_scene_to_file("res://scenes/ConstellationSelection.tscn")
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
